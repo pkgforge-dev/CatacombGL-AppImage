981c903edd9e75a4a738941fc8323a105bb9ae71
Link4Electronics <link4electronics@gmail.com>
2026-08-19 01:53:12 -0300

    Update get-dependencies.sh

--- get-dependencies.sh
+++ get-dependencies.sh
@@ -15,10 +15,6 @@ echo "Installing debloated packages..."
 echo "---------------------------------------------------------------"
 get-debloated-pkgs --add-common --prefer-nano
 
-# Comment this out if you need an AUR package
-#make-aur-package PACKAGENAME
-
-# If the application needs to be manually built that has to be done down here
 echo "Building CatacombGL..."
 echo "---------------------------------------------------------------"
 REPO="https://github.com/ArnoAnsems/CatacombGL"
